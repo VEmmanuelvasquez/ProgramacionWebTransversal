c8759b8eb75fabdde3dd596c5072e3ebdc722aca
chichoinf@gmail.com <chichoinf@gmail.com>
2025-11-12 18:36:56 -0300

    arreglo de la imagenes carrusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-const nombres = ["Unit-734", "Unit-735", "Unit-736","Unit-737"];
-const enlaces = ["unit734.html", "unit735.html", "unit736.html","unit737.html"];
 
-const imagenes = document.querySelectorAll(".slider-img");
+
+
+
+
+const nombres = ["Unit-734", "Unit-735", "Unit-736", "Unit-737"];
+const enlaces = ["unit734.html", "unit735.html", "unit736.html", "unit737.html"];
+const rutas = [
+  "./assets/img/slider/robot1.png",
+  "./assets/img/slider/robot2.png",
+  "./assets/img/slider/robot3.png",
+  "./assets/img/slider/robot4.png"
+];
+
+const imagen = document.getElementById("imagen_robot");
 const nombreRobot = document.getElementById("nombre_robot");
 const botonExplorar = document.getElementById("boton_explorar");
 const atras = document.querySelector(".atras");
@@ -9,27 +20,20 @@ const adelante = document.querySelector(".adelante");
 
 let indice = 0;
 
-function mostrarImagen() {
-  imagenes.forEach((img, i) => {
-    img.style.display = i === indice ? "block" : "none";
-  });
-
+function mostrarRobot() {
+  imagen.src = rutas[indice];
   nombreRobot.textContent = nombres[indice];
   botonExplorar.href = enlaces[indice];
 }
 
 atras.addEventListener("click", () => {
-  indice = (indice - 1 + imagenes.length) % imagenes.length;
-  mostrarImagen();
+  indice = (indice - 1 + rutas.length) % rutas.length;
+  mostrarRobot();
 });
 
 adelante.addEventListener("click", () => {
-  indice = (indice + 1) % imagenes.length;
-  mostrarImagen();
+  indice = (indice + 1) % rutas.length;
+  mostrarRobot();
 });
 
-mostrarImagen(); 
-
-
-
-
+mostrarRobot();
